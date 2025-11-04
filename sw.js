8cbf3f1bb8160634d9c3182ff4a7d8224d9641e6
molnartom1 <molnar.tamas7@icloud.com>
2025-11-04 15:12:18 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'family-board-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.webmanifest',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
