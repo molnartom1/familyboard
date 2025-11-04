08af95788894d9db51dbdd296db14e5b16e45dfc
molnartom1 <molnar.tamas7@icloud.com>
2025-11-04 15:24:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,34 @@
-const CACHE_NAME = 'family-board-v2';
+const CACHENAME = 'family-board-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/manifest.webmanifest',
-  '/icon-192.png',
-  '/icon-512.png'
+  'index.html',
+  'style.css',
+  'manifest.webmanifest',
+  'icon-192.png',
+  'icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(CACHENAME)
       .then(cache => cache.addAll(urlsToCache))
       .then(() => self.skipWaiting())
   );
 });
+
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHENAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
